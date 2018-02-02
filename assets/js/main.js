bf3732c1772308a8dd9d8f1c894ee09929e5748a
Piotr Kulpa <p.kulpa@onet.eu>
2018-02-02 11:36:26 +0100

    works + links

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,11 @@
 
   var menu = $('.nav-inner'),
       menuLinks = $('.nav-close a'),
-      drawer = $('.nav-close');
+      drawer = $('.nav-close'),
+      workList = $('.work-list'),
+      workListInner = $('.work-list-inner')
+      messageMe = $('.message-me')
+      messageMeTxt = $('.message-me-txt');
 
       menu.on('click', function (e) {
         drawer.toggleClass('nav-open');
@@ -18,12 +22,41 @@
         drawer.toggleClass('nav-open');
       });
 
-      //menu scroll system
+      /*
+       * menu scroll system
+       */
+
       drawer.on('click', 'a', function(e){
         var position = $(this).data('position');
         $('html, body').animate({
           scrollTop: $(position).offset().top
         }, 'slow');
     	});
+
+      /*
+       * work list system
+       */
+
+       workList.on('click', '.work-list-inner', function(e){
+         workListInner.removeClass('work-list-click');
+         $( this ).toggleClass('work-list-click');
+         $('.work-tools-inner').hide();
+         var getWork = $( this ).data('work');
+         $(getWork).show();
+         $(getWork).addClass('text-focus-in');
+     	});
+
+      /*
+       * work list system
+       */
+
+        messageMe.on('click', function(e){
+
+
+        messageMeTxt.show();
+        messageMeTxt.addClass('text-focus-in');
+     });
+
+
 
 } () );
